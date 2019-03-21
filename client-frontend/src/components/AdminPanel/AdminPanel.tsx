a63a164cb51ebffd0adc45c086d9e7cbcace7bc9
tbarnat <tomasz.barnat@simplito.com>
2019-03-21 09:40:40 +0100

    it's all about small things

--- client-frontend/src/components/AdminPanel/AdminPanel.tsx
+++ client-frontend/src/components/AdminPanel/AdminPanel.tsx
@@ -4,8 +4,25 @@ import classes from './AdminPanel.module.scss';
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 import Button from "react-bootstrap/Button";
+import {IHorseRidingHourQ} from "../../DataModel";
+import update from "immutability-helper";
 
-class App extends React.Component {
+class App extends React.Component<any, any> {
+
+  constructor(props: any) {
+    super(props)
+    this.state = {
+      input:''
+    }
+  }
+
+  changeInputHandler = (e: any) => {
+    this.setState({input: e.target.value})
+    console.log(e.target.value)
+    //window.hmClient
+    //
+  }
+
   render() {
     return (
       <div className={classes.AdminPanel}>
@@ -14,10 +31,10 @@ class App extends React.Component {
             Labelka
           </Col>
           <Col className={classes.AutocompleteSelectOne}>
-            <select/>
+            <input value={this.state.input} onChange={(e: any) => this.changeInputHandler(e)}/>
           </Col>
           <Col>
-            <Button color="primary" variant="primary" onClick={() => console.log('new')}>Nowy</Button>
+            <Button color="primary" variant="primary" onClick={() => console.log('new')}>Utwórz</Button>
           </Col>
           <Col>
             <Button color="primary" variant="primary" onClick={() => console.log('edit')}>Edytuj</Button>
