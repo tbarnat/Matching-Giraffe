c0be9ce30faf17417fdd09a893bcc53cb5786f8a
tbarnat <tbarnat@wp.pl>
2019-03-27 20:26:29 +0100

    admin panel clearing

--- client-frontend/src/components/AdminPanel/AdminPanel.tsx
+++ client-frontend/src/components/AdminPanel/AdminPanel.tsx
@@ -4,9 +4,9 @@ import classes from './AdminPanel.module.scss';
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 import Button from "react-bootstrap/Button";
-import { IBackendMsg } from "../../App";
-import { ActionInMsg } from "../../Client";
-import { Typeahead } from 'react-bootstrap-typeahead';
+import {IBackendMsg} from "../../App";
+import {ActionInMsg} from "../../Client";
+import {Typeahead} from 'react-bootstrap-typeahead';
 
 class App extends React.Component<any, any> {
 
@@ -15,6 +15,7 @@ class App extends React.Component<any, any> {
   *  -> buttons reveals the further options
   * */
   private objectTypes = ['horse', 'kid', 'trainer']
+  private typeAhead: { [name: string]: any } = {}
 
   constructor(props: any) {
     super(props)
@@ -52,7 +53,7 @@ class App extends React.Component<any, any> {
       options[keyBezS].sort()
     })
     //console.log(options)
-    this.setState({ options })
+    this.setState({options})
   }
 
   private getReqName(stateObjectName: string): ActionInMsg {
@@ -71,19 +72,27 @@ class App extends React.Component<any, any> {
     let currInput = ''
     if (Array.isArray(e) && e[0]) {
       currInput = e[0]
-      this.setState({ [fieldName]: { input: currInput }, })
+      this.setState({[fieldName]: {input: currInput},})
     }
-    this.setState({ active: fieldName })
-    let getResponse: IBackendMsg = await window.hmClient.sendAndWait(this.getReqName(fieldName), { name: currInput })
-    this.setState({ [fieldName]: { existingEntry: getResponse.success } })
+    this.setState({active: fieldName})
+    let getResponse: IBackendMsg = await window.hmClient.sendAndWait(this.getReqName(fieldName), {name: currInput})
+    this.setState({[fieldName]: {existingEntry: getResponse.success}})
   }
 
   onFocusHandler = (fieldName: string) => {
-    this.setState({ active: fieldName })
+    this.setState({active: fieldName})
     for (let typeName of this.objectTypes) {
       if (typeName != fieldName) {
-        this.setState({ [typeName]: { input: [] }, })
+        this.typeAhead[typeName].getInstance().clear()
+        this.setState({[typeName]: {input: []},})
       }
+    }
+  }
+
+  getAdditionalFormForEntry(name:string){
+    if(this.state.active == name){
+
+      return(<input/>)
     }
   }
 
@@ -93,18 +102,18 @@ class App extends React.Component<any, any> {
       <div className={classes.AdminPanelRow} key={'costam'}>
         <Row>
           <Col className={classes.Labels}>Dane 1</Col>
-          <Col className={classes.Labels}><input /></Col>
-          <Col /><Col />
+          <Col className={classes.Labels}><input/></Col>
+          <Col/><Col/>
         </Row>
         <Row>
           <Col className={classes.Labels}>Dane 2</Col>
-          <Col className={classes.Labels}><input /></Col>
-          <Col /><Col />
+          <Col className={classes.Labels}><input/></Col>
+          <Col/><Col/>
         </Row>
       </div>
     )
 
-    const types = [{ type: 'kid', label: 'Bachory' }, { type: 'horse', label: 'Horsesy' }, {
+    const types = [{type: 'kid', label: 'Bachory'}, {type: 'horse', label: 'Horsesy'}, {
       type: 'trainer',
       label: 'Kadra'
     },]
@@ -112,14 +121,14 @@ class App extends React.Component<any, any> {
       let name = row.type
       console.log(row)
       return (
-        <div className={classes.AdminPanelRow} key={row.type + '_adm'} style={{ width: 1000 }}>
+        <div className={classes.AdminPanelRow} key={row.type + '_adm'} style={{width: 1000}}>
           <Row>
             <Col className={classes.Labels}>
               <strong>{row.label}</strong>
             </Col>
-            <Col /><Col />
+            <Col/><Col/>
           </Row>
-          <hr />
+          <hr/>
           <Row>
             <Col>Nazwa</Col>
             <Col className={classes.AutocompleteSelectOne}>
@@ -131,7 +140,7 @@ class App extends React.Component<any, any> {
                 key={row.type + '_typeahead_adm'}
                 placeholder={row.label}
                 onChange={(e: any) => this.changeTypeaheadHandler(e, name)}
-                onFocus={() => this.onFocusHandler(row.type)}
+                onFocus={() => this.onFocusHandler(name)}
                 options={this.state.options[name]}
                 // selected={this.state[name].input}
                 allowNew={true}
@@ -140,33 +149,40 @@ class App extends React.Component<any, any> {
                 inputProps={{
                   width: '20px'
                 }}
+                ref={(ref) => this.typeAhead[name] = ref}
               />
             </Col>
-            <Col /><Col />
+            <Col/><Col/>
           </Row>
+          <Col/>
+          <Col>
+          {this.getAdditionalFormForEntry(name)}
+          </Col>
           {/*{content}*/}
-          <br />
+          <Col/><Col/>
+          <br/>
           <Row>
-            <Col /><Col />
+            <Col/><Col/>
             <Col>
               <Row>
 
                 <Col><Button variant="secondary" onClick={() => console.log('new ' + name)}
-                  disabled={this.state[row.type].existingEntry || (this.state.active != name)}>
+                             disabled={this.state[row.type].existingEntry || (this.state.active != name)}>
                   Utwórz</Button></Col>
                 <Col><Button variant="secondary" onClick={() => console.log('edit ' + name)}
-                  disabled={!this.state[row.type].existingEntry || (this.state.active != name)}>
+                             disabled={!this.state[row.type].existingEntry || (this.state.active != name)}>
                   Edytuj</Button></Col>
-                <span />
+                <span/>
                 <Col><Button variant="secondary" onClick={() => console.log('remove ' + name)}
-                  disabled={!this.state[row.type].existingEntry || (this.state.active != name)}>
+                             disabled={!this.state[row.type].existingEntry || (this.state.active != name)}>
                   Usuń</Button></Col>
+                {/*todo remember to call get list after any button is pressed*/}
 
               </Row>
             </Col>
           </Row>
-          <br />
-          <hr />
+          <br/>
+          <hr/>
         </div>
       );
     })
