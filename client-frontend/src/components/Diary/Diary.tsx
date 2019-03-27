6ca4f47c2449499ab60bd294fb413f1646f131f1
tulodzieckim <corwin1519@gmail.com>
2019-03-27 19:35:26 +0100

    set diary styles same as dayplan

--- client-frontend/src/components/Diary/Diary.tsx
+++ client-frontend/src/components/Diary/Diary.tsx
@@ -11,117 +11,116 @@ import { Typeahead } from 'react-bootstrap-typeahead';
 
 import { IHorseRidingDayQ, IHorseRidingHourQ } from '../../DataModel';
 import classes from './Diary.module.scss';
-import { RouteComponentProps } from "react-router-dom";
 
 
-interface IProps extends RouteComponentProps<any> {
-  chosendate: string;
+interface IState extends IHorseRidingDayQ {
+
+  options?: {
+    kid?: { id: number, label: string }[] | string[],
+    trainer?: { id: number, label: string }[] | string[],
+    horse?: { id: number, label: string }[] | string[],
+  }
 }
 
-class Diary extends React.Component<IProps, IHorseRidingDayQ> {
 
-   constructor(props: IProps) {
-    super(props)
-    this.state = {
-      day: '',
-      remarks: '',
-      dailyExcludes: [],
-      hours: [
-        {
-          hour: '1230',
-          trainer: ['Paulina'],
-          trainingsDetails: [
-            { kidName: 'Julka Mala', horse: '' },
-            { kidName: 'Maja' },
-            { kidName: 'Julka Lonza' },
-            { kidName: 'Ola C' },
-            { kidName: '' },
-          ]
-        },
-        {
-          hour: '1430',
-          trainer: ['Eva'],
-          trainingsDetails: [
-            { kidName: 'Ola C' },
-            { kidName: 'Weronika' },
-            { kidName: 'Emilka' },
-            { kidName: 'Kalina' },
-            { kidName: 'Paula' },
-          ]
-        },
-        {
-          hour: '1530',
-          trainer: ['Eva'],
-          trainingsDetails: [
-            { kidName: 'Paula' },
-            { kidName: 'Kalina' },
-          ]
-        },
-      ],
-    }
+class DayPlan extends React.Component<any, any> {
+  state = {
+    day: '2019-01-08',
+    remarks: 'Bla bla',
+    dailyExcludes: ['Bracio'],
+    hours: [
+      {
+        hour: '1200',
+        trainer: ['Eva'],
+        trainingsDetails: [
+          { kidName: 'Agnieszka E', horse: 'Bella' },
+          { kidName: 'Julka R' },
+          { kidName: 'Ania P.' },
+          { kidName: undefined },
+        ]
+      },
+    ],
   };
 
 
   componentDidMount() {
-    console.log(this.props)
-     const param = this.props.match.params.chosendate;
-     //TODO get data from API based on params (np. 20190203)
+    // this.init();
   }
 
   render() {
+    console.log('=======================')
+    console.log('STATE: ', this.state)
     const hours = this.state.hours.map((hour, hourIndex) => {
       const kids = hour.trainingsDetails.map((training, trainingIndex) => {
         return (
-          <Form.Control
-            key={trainingIndex}
-            value={training.kidName}
-            readOnly
-          />
+          <div className={classes.KidAndButtonContainer} key={trainingIndex}
+          >
+            <Typeahead
+              id={trainingIndex}
+              placeholder="Dziecko"
+              options={[]}
+              selected={training.kidName === undefined ? [] : [training.kidName]}
+              inputProps={{
+                width: '20px'
+              }}
+              disabled
+            />
+          </div>
         )
       })
 
       const horses = hour.trainingsDetails.map((training, trainingIndex) => {
         return training.kidName
           ? (
-            <Form.Control
+            <Typeahead
               key={trainingIndex}
-              value={training.horse}
-              readOnly
+              id={trainingIndex}
+              placeholder="Koń"
+              options={[]}
+              selected={training.horse ? [training.horse] : []}
+              disabled
             />
           ) : null;
       })
 
-      const trainers = hour.trainer.map((tr, trainerIndex) => {
-        return (
-          <Form.Control
-            key={trainerIndex}
-            value={tr}
-            readOnly
-          />
-        )
-      })
+
+      const trainers = (
+        <Typeahead
+          placeholder="Trenerzy"
+          id={`Trainers-${hourIndex}`}
+          options={[]}
+          selected={this.state.hours[hourIndex].trainer}
+          multiple
+          emptyLabel="Brak wyników"
+          disabled
+        />
+      )
 
       return (
         <Card key={hourIndex} className={classes.OneHour}>
           <Row>
-            <Col className={classes.Hours}>
-              <span className={[classes.HourLabel].join(' ')}>Godzina</span>
-              <Form.Control
-                value={hour.hour}
-                readOnly
-              />
+            <Col>
+              <div className={[classes.Hours, classes.LabelSection].join(' ')}>
+                <span className={classes.Label}>Godzina</span>
+                <Form.Control
+                  // label="Godzina"
+                  placeholder="Godzina"
+                  value={hour.hour}
+                  disabled
+                />
+              </div>
+              <div className={[classes.Trainers, classes.LabelSection].join(' ')}>
+                <span className={classes.Label}>Trenerzy</span>
+                {trainers}
+              </div>
             </Col>
-            <Col className={classes.Kids}>
-              <span className={[classes.KidLabel].join(' ')}>Gówniaki</span>
+            <Col className={[classes.Kids, classes.LabelSection].join(' ')}>
+              <span className={classes.Label}>Gówniaki</span>
               {kids}
             </Col>
-            <Col className={classes.Horses}>
-              <span className={[classes.HorseLabel].join(' ')}>Konie</span>
+            <Col className={[classes.Horses, classes.LabelSection].join(' ')}>
+              <span className={classes.Label}>Konie</span>
               {horses}
-            </Col>
-            <Col className={classes.Trainers}>
-              <span className={[classes.TrainerLabel].join(' ')}>Trenerzy</span>
-              {trainers}
             </Col>
           </Row>
         </Card>
@@ -133,25 +132,33 @@ class Diary extends React.Component<IProps, IHorseRidingDayQ> {
     return (
       <Container className={classes.DayPlan} fluid>
         <Row>
-          <Col>
-            <span className={[classes.DayLabel].join(' ')}>Dzień</span>
+          <Col className={classes.LabelSection}>
+            <span className={classes.Label}>Dzień</span>
             <Form.Control
+              placeholder="Dzień"
               value={this.state.day}
-              readOnly
+              disabled
             />
           </Col>
-          <Col>
-            <span className={[classes.DailyExcludesLabel].join(' ')}>Wyłączone konie</span>
-            <Form.Control
-              value={'TODO multiselect'}
-              readOnly
+          <Col className={classes.LabelSection}>
+            <span className={classes.Label}>Wyłączone konie</span>
+            <Typeahead
+              placeholder="Wyłączone konie"
+              id={'dailyExcludes'}
+              onChange={(e: any) => this.setState({ dailyExcludes: e })}
+              options={[]}
+              selected={this.state.dailyExcludes}
+              multiple
+              emptyLabel="Brak wyników"
+              disabled
             />
           </Col>
-          <Col>
-            <span className={[classes.RemarksLabel].join(' ')}>Uwagi</span>
+          <Col className={classes.LabelSection}>
+            <span className={classes.Label}>Uwagi</span>
             <Form.Control
+              placeholder="Uwagi"
               value={this.state.remarks}
-              readOnly
+              disabled
             />
           </Col>
         </Row>
@@ -159,10 +166,10 @@ class Diary extends React.Component<IProps, IHorseRidingDayQ> {
         {hours}
         <Button color="primary" variant="primary" onClick={() => console.log(this.state)}>get state</Button>
         <Button color="orange" variant="secondary" onClick={() => console.log(this.state.hours[0].trainingsDetails)}>get hours</Button>
-      </Container>
+      </Container >
     )
 
   }
 };
 
-export default Diary;
+export default DayPlan;
