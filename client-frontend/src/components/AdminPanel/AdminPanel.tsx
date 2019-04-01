27f82eb69032bffc6e58530447d821528360b662
tbarnat <tomasz.barnat@simplito.com>
2019-04-01 16:14:48 +0200

    drag and drop first try

--- client-frontend/src/components/AdminPanel/AdminPanel.tsx
+++ client-frontend/src/components/AdminPanel/AdminPanel.tsx
@@ -8,6 +8,40 @@ import Form from 'react-bootstrap/Form';
 import {IBackendMsg} from "../../App";
 import {ActionInMsg} from "../../Client";
 import {Typeahead} from 'react-bootstrap-typeahead';
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
+
+
+// fake data generator
+const getItems = (count: any) =>
+  Array.from({ length: count }, (v, k) => k).map(k => ({
+    id: `item-${k}`,
+    content: `item ${k}`,
+  }));
+
+const grid = 8;
+
+const getItemStyle = (isDragging: any, draggableStyle: any) => ({
+  // some basic styles to make the items look a bit nicer
+  userSelect: 'none',
+  padding: grid * 2,
+  margin: `0 ${grid}px 0 0`,
+
+  // change background colour if dragging
+  background: isDragging ? 'lightgreen' : 'grey',
+
+  // styles we need to apply on draggables
+  ...draggableStyle,
+});
+
+const getListStyle = (isDraggingOver: any) => ({
+  background: isDraggingOver ? 'lightblue' : 'lightgrey',
+  display: 'flex',
+  padding: grid,
+  overflow: 'auto',
+});
+
+
+//todo podczytywanie wartosci do formularza
 
 class App extends React.Component<any, any> {
 
@@ -17,6 +51,7 @@ class App extends React.Component<any, any> {
   * */
   private objectTypes = ['kid', 'horse', 'trainer']
   private typeAhead: { [name: string]: any } = {}
+  private tempDndContent = {best:['Bucefał'],nice:['Grzmot'],isok:[],limp:['Kumak'],excl:[]}
 
   constructor(props: any) {
     super(props)
@@ -41,7 +76,8 @@ class App extends React.Component<any, any> {
         existingEntry: false,
         selected: {}
       },
-      active: undefined
+      active: undefined,
+      items: getItems(6)
     }
     this.init()
   }
@@ -119,7 +155,38 @@ class App extends React.Component<any, any> {
         </Form.Group>
       )
     }
-    console.log(this.state['kid'])
+    let prefs = (
+      <DragDropContext onDragEnd={this.onDragEnd}>
+        <Droppable droppableId="droppable" direction="horizontal">
+          {(provided, snapshot) => (
+            <div
+              ref={provided.innerRef}
+              style={getListStyle(snapshot.isDraggingOver)}
+              {...provided.droppableProps}
+            >
+              {this.state.items.map((item: any, index: any) => (
+                <Draggable key={item.id} draggableId={item.id} index={index}>
+                  {(provided, snapshot) => (
+                    <div
+                      ref={provided.innerRef}
+                      {...provided.draggableProps}
+                      {...provided.dragHandleProps}
+                      style={getItemStyle(
+                        snapshot.isDragging,
+                        provided.draggableProps.style
+                      )}
+                    >
+                      {item.content}
+                    </div>
+                  )}
+                </Draggable>
+              ))}
+              {provided.placeholder}
+            </div>
+          )}
+        </Droppable>
+      </DragDropContext>
+    );
     return (
       <Form>
         {newName}
@@ -127,9 +194,36 @@ class App extends React.Component<any, any> {
           <Form.Label>Uwagi (opcjonalnie)</Form.Label>
           <Form.Control as="textarea" rows={'1'}/>
         </Form.Group>
+        {prefs}
       </Form>
     )
   }
+
+  onDragEnd(result: any) {
+    // dropped outside the list
+    if (!result.destination) {
+      return;
+    }
+
+    /*const items = this.reorder(
+      this.state.items,
+      result.source.index,
+      result.destination.index
+    );*/
+
+    /*this.setState({
+      items,
+    });*/
+  }
+
+  // a little function to help us with reordering the result
+  reorder = (list: any, startIndex: any, endIndex: any) => {
+    const result = Array.from(list);
+    const [removed] = result.splice(startIndex, 1);
+    result.splice(endIndex, 0, removed);
+
+    return result;
+  };
 
   getHorseForm() {
     let newName
